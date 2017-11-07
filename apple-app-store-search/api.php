a13981a7121343a900218a401d55d70284eb8bf5
Slavik Nychkalo <bboyheadman@gmail.com>
2017-11-07 10:40:07 +0200

    Update api.php

--- apple-app-store-search/api.php
+++ apple-app-store-search/api.php
@@ -21,11 +21,9 @@ if (isset($_GET['search'], $_GET['country'])) {
 	$context = stream_context_create($opts);
 
 	// Open the file using the HTTP headers set above
-	$file = file_get_contents("https://search.itunes.apple.com/WebObjects/MZStore.woa/wa/search?clientApplication=Software&term={$term}&caller=com.apple.AppStore&version=1",
-							  false, $context);
+	$file = file_get_contents("https://search.itunes.apple.com/WebObjects/MZStore.woa/wa/search?clientApplication=Software&term={$term}&caller=com.apple.AppStore&version=1", false, $context);
 
 	echo $file;
-	
 } else if (isset($_GET['appid'], $_GET['country'])) {
 	$appid = $_GET['appid'];
 	$country = $countries[$_GET['country']];
@@ -46,4 +44,3 @@ if (isset($_GET['search'], $_GET['country'])) {
 
 	echo $file;
 }
-?>
