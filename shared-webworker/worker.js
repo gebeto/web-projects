9caf4546f2106cfecc0264b7ede1c37e959ceb05
gebeto <slavik.nychkalo@gmail.com>
2018-02-14 13:31:47 +0200

    upd

--- shared-webworker/worker.js
+++ shared-webworker/worker.js
@@ -8,14 +8,14 @@ var id, port;
 addEventListener('connect', function(e) {
 	let port = e.ports[0];
 	// port.start();
-	port.postMessage({title: 'init', isRan: isRan});
+	// port.postMessage({title: 'init', isRan: isRan});
 	cache.push(port);
 	
 	if (!isRan) {
 		isRan = true;
 		setInterval(function () {
 			cache.map(function (prt) {
-				prt.postMessage({title: count, isRan: isRan});
+				prt.postMessage({title: count, isRan: isRan, name: name});
 			})
 			count++;
 		}, 2000);
