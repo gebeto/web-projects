1dad5703e6e86803426c4e86932d6a3d9ea6fdb8
gebeto <slavik.nychkalo@gmail.com>
2018-03-18 16:27:32 +0200

    update gradients page

--- gradients/src/card.js
+++ gradients/src/card.js
@@ -1,28 +1,28 @@
 import { cards } from './index';
 
 export function getHue(red, green, blue) {
-    var min = Math.min(Math.min(red, green), blue);
-    var max = Math.max(Math.max(red, green), blue);
+	var min = Math.min(Math.min(red, green), blue);
+	var max = Math.max(Math.max(red, green), blue);
 
-    if (min == max) {
-        return 0;
-    }
+	if (min == max) {
+		return 0;
+	}
 
-    var hue = 0.0;
-    if (max == red) {
-        hue = (green - blue) / (max - min);
+	var hue = 0.0;
+	if (max == red) {
+		hue = (green - blue) / (max - min);
 
-    } else if (max == green) {
-        hue = 2.0 + (blue - red) / (max - min);
+	} else if (max == green) {
+		hue = 2.0 + (blue - red) / (max - min);
 
-    } else {
-        hue = 4.0 + (red - green) / (max - min);
-    }
+	} else {
+		hue = 4.0 + (red - green) / (max - min);
+	}
 
-    hue = hue * 60;
-    if (hue < 0) hue = hue + 360;
+	hue = hue * 60;
+	if (hue < 0) hue = hue + 360;
 
-    return Math.round(hue);
+	return Math.round(hue);
 }
 
 export function createCard(colorStart, colorEnd, name) {
@@ -40,10 +40,23 @@ export function createCard(colorStart, colorEnd, name) {
 	console.log(d, lum)
 
 	const card = document.createElement('div');
-	card.style.width = '240px';
-	card.style.height = '270px';
+	// card.style.width = '240px';
+	// card.style.height = '270px';
 	card.style.display = 'inline-block';
-	card.innerHTML = `
+	card.style.margin = '6px';
+	card.innerHTML = cardOld(colorStart, colorEnd, name);
+	// card.innerHTML = cardNew(colorStart, colorEnd, name);
+	cards.push({
+		block: card,
+		hue: d,
+		lum: lum,
+	});
+	return card;
+}
+
+function cardOld(colorStart, colorEnd, name) {
+				// <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="linearGradient-${name}">
+	return `
 		<svg class="card" width="240px" height="270px" viewBox="0 0 240 270" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 			<defs>
 				<rect id="path-1" x="0" y="0" width="200" height="230" rx="16"></rect>
@@ -52,7 +65,7 @@ export function createCard(colorStart, colorEnd, name) {
 					<feGaussianBlur stdDeviation="10" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
 					<feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.05 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
 				</filter>
-				<linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="linearGradient-${name}">
+				<linearGradient x1="0%" y1="50%" x2="100%" y2="50%" id="linearGradient-${name}">
 					<stop stop-color="${colorStart}" offset="0%"></stop>
 					<stop stop-color="${colorEnd}" offset="100%"></stop>
 				</linearGradient>
@@ -79,10 +92,59 @@ export function createCard(colorStart, colorEnd, name) {
 			</g>
 		</svg>
 	`;
-	cards.push({
-		block: card,
-		hue: d,
-		lum: lum,
-	});
-	return card;
+}
+
+function cardNew(colorStart, colorEnd, name) {
+	colorStart = colorStart.replace('#', '');
+	colorEnd = colorEnd.replace('#', '');
+	return `<svg width="247px" height="72px" viewBox="0 0 247 72" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+		<defs>
+			<rect id="path-1" x="0" y="0" width="235" height="60" rx="5"></rect>
+			<filter x="-4.7%" y="-11.7%" width="109.4%" height="136.7%" filterUnits="objectBoundingBox" id="filter-2">
+				<feOffset dx="0" dy="4" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+				<feGaussianBlur stdDeviation="3" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+				<feColorMatrix values="0 0 0 0 0.290196078   0 0 0 0 0.290196078   0 0 0 0 0.290196078  0 0 0 0.1 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
+			</filter>
+			<circle id="path-3" cx="157.5" cy="41.5" r="2.5"></circle>
+			<circle id="path-4" cx="157.5" cy="18.5" r="2.5"></circle>
+			<linearGradient x1="0%" y1="100%" x2="100%" y2="0%" id="linearGradient-${name}">
+				<stop stop-color="#${colorStart}" offset="0%"></stop>
+				<stop stop-color="#${colorEnd}" offset="100%"></stop>
+			</linearGradient>
+			<path d="M0,4.99952863 C0,2.23836521 2.24153039,0 5.00329329,0 L140,0 L140,60 L5.00329329,60 C2.24005071,60 0,57.7574344 0,55.0004714 L0,4.99952863 Z" id="path-6"></path>
+		</defs>
+		<g id="Gradients" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+			<g id="List-gradients" transform="translate(-19.000000, -25.000000)">
+				<g id="1-item" transform="translate(25.000000, 27.000000)">
+					<g id="body">
+						<use fill="black" fill-opacity="1" filter="url(#filter-2)" xlink:href="#path-1"></use>
+						<use fill="#FFFFFF" fill-rule="evenodd" xlink:href="#path-1"></use>
+					</g>
+					<circle id="border-2" fill="#C5C5C5" cx="157.5" cy="41.5" r="5.5"></circle>
+					<circle id="border-1" fill="#C5C5C5" cx="157.5" cy="18.5" r="5.5"></circle>
+					<rect id="-hr-" fill="#C5C5C5" x="157" y="22" width="1" height="16"></rect>
+					<text id="#${colorStart}" font-family=".AppleSystemUIFont" font-size="12" font-weight="normal">
+						<tspan x="166" y="45" fill="#C5C5C5">#</tspan>
+						<tspan x="173.59375" y="45" fill="#4A4A4A">${colorStart}</tspan>
+					</text>
+					<g id="CIR-COLOR-2">
+						<use fill="#${colorStart}" fill-rule="evenodd" xlink:href="#path-3"></use>
+						<circle stroke="#FFFFFF" stroke-width="2" cx="157.5" cy="41.5" r="3.5"></circle>
+					</g>
+					<text id="#${colorEnd}" font-family=".AppleSystemUIFont" font-size="12" font-weight="normal">
+						<tspan x="166" y="22" fill="#C5C5C5">#</tspan>
+						<tspan x="173.59375" y="22" fill="#4A4A4A">${colorEnd}</tspan>
+					</text>
+					<g id="CIR-COLOR-1">
+						<use fill="#${colorEnd}" fill-rule="evenodd" xlink:href="#path-4"></use>
+						<circle stroke="#FFFFFF" stroke-width="2" cx="157.5" cy="18.5" r="3.5"></circle>
+					</g>
+					<mask id="mask-7" fill="white">
+						<use xlink:href="#path-6"></use>
+					</mask>
+					<use id="gradient" fill="url(#linearGradient-${name})" xlink:href="#path-6"></use>
+				</g>
+			</g>
+		</g>
+	</svg>`;
 }
--- gradients/src/index.js
+++ gradients/src/index.js
@@ -18,10 +18,42 @@ const gradients = [
 	['#F8BF66', '#F95C81'],
 	['#77EFD8', '#45B9F1'],
 	['#62A8FD', '#9E80F2'],
+
+	// ['#90228C', '#009B90'],
+	// ['#00ABEE', '#F8F8BA'],
+	// ['#313131', '#534D4D'],
+	// ['#421057', '#F45509'],
+	// ['#EC4443', '#E9BB85'],
+	// ['#D481FF', '#06F0E0'],
+	// ['#F298E7', '#A5F8F8'],
+	// ['#846FF4', '#F17674'],
+	// ['#00F198', '#08F6F6'],
+	// ['#7A7A7A', '#E3DFDF'],
+	// ['#AB64F5', '#62DBF6'],
+	// ['#009900', '#FFFF92'],
+	// ['#2E266F', '#7C2289'],
+	// ['#F38731', '#D8DF16'],
+	// ['#CF396F', '#F6E28B'],
+	// ['#04A5C0', '#F9F981'],
+	// ['#EB121A', '#F9EB19'],
+	// ['#62258A', '#FC0F77'],
+	// ['#06974A', '#F2E41F'],
+	// ['#D20B54', '#FFB84A'],
+	// ['#303395', '#28EFEF'],
 ];
 
+// cards.sort((a, b) => a.hue - b.hue).map((c) => {
+// 	wrapper.removeChild(c.block);
+// 	wrapper.appendChild(c.block);
+// });
+
 const wrapper = document.querySelector('.wrapper');
-gradients.forEach((grad, i) => wrapper.appendChild(createCard(grad[0], grad[1], i)));
+const c = gradients.map((grad, i) => wrapper.appendChild(createCard(grad[0], grad[1], i)));
+// cards.sort((a, b) => a.hue - b.hue).map((c) => {
+// 	wrapper.removeChild(c.block);
+// 	wrapper.appendChild(c.block);
+// });
+
 
 // for (var i = 0; i < grads.length; i++) {
 // 	wrapper.appendChild(createCard(grads[i][0], grads[i][1], i));
