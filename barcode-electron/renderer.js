8bd3accdcb31219f2453140bf3b03710b5413b3a
gebeto <slavik.nychkalo@gmail.com>
2018-05-19 01:49:10 +0300

    upd

--- barcode-electron/renderer.js
+++ barcode-electron/renderer.js
@@ -2,9 +2,12 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+const checkIncludes = require("./google.js");
+
 const input = document.querySelector("input");
 document.querySelector("form").addEventListener("submit", function(e) {
 	e.preventDefault();
+	checkIncludes(input.value);
 	input.placeholder = input.value;
 	input.value = "";
 });
