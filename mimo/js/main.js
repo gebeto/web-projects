ac9c8831ad31a59070e7905f1b350e4b97b89c57
gebeto <slavik.nychkalo@gmail.com>
2018-10-22 11:26:58 +0300

    move from gebeto.github.io to web-projects

---
+++ mimo/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$.ajax({
+		url: 'tutorials/tutorials.json',
+		type: 'GET',
+		dataType: 'json',
+	})
+	.done(function(res) {
+		console.log(res);
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+});
