fe47539cdd02f0f4b570c0626ff38f1ac0077f70
gebeto <slavik.nychkalo@gmail.com>
2019-02-06 19:18:35 +0200

    migrate from webpack 3 to webpack 4

--- space/webpack.config.js
+++ space/webpack.config.js
@@ -3,51 +3,45 @@ const path = require('path');
 const fs = require('fs');
 
 const config = {
-	// mode: 'production',
 	entry: "./src/index.ts",
 	output: {
-		filename: "[name].bundle.js",
+		filename: "bundle.js",
 		path: path.resolve(__dirname, 'dist'),
 	},
 	resolve: {
 		extensions: [".ts", ".tsx", ".js"]
 	},
 	module: {
-		loaders: [
+		rules: [
 			{
 				test: /\.ts$/,
 				loader: "awesome-typescript-loader"
 			},
 		]
 	},
+
 	devServer: {
 		contentBase: path.join(__dirname, "dist"),
 		compress: true,
 		port: 9000,
 		open: true,
 	},
+
 	plugins: []
 };
 
-if (process.env.npm_lifecycle_event === 'build:ggi') {
-	const newOutpupPath = path.resolve(__dirname, '../../gebeto.github.io/for-fun/space');
-	if (fs.existsSync(newOutpupPath)) {
-		config.output.path = newOutpupPath;
-	}
-}
-
-if (process.env.npm_lifecycle_event.match(/^build/)) {
-	config.plugins.push(
-		new UglifyJsPlugin({
-			uglifyOptions: {
-				warnings: false,
-				unsafe: true,
-				compress: {
-					drop_console: true
-				}
-			}
-		})
-	);
-}
+// if (process.env.npm_lifecycle_event.match(/^build/)) {
+// 	config.plugins.push(
+// 		new UglifyJsPlugin({
+// 			uglifyOptions: {
+// 				warnings: false,
+// 				unsafe: true,
+// 				compress: {
+// 					drop_console: true
+// 				}
+// 			}
+// 		})
+// 	);
+// }
 
 module.exports = config;
