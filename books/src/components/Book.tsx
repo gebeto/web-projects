cfd316b3143a06a7d6bcacbdded2f7f97e4af250
gebeto <slavik.nychkalo@gmail.com>
2019-02-08 22:31:50 +0200

    add list of tracks

--- books/src/components/Book.tsx
+++ books/src/components/Book.tsx
@@ -6,9 +6,17 @@ import JSONTree from 'react-json-tree'
 
 class Book extends React.Component<any, any> {
 	render() {
+		const { book = {} } = this.props;
+		const { TracksListTitle, MainTitle, TracksList = [] } = book;
 		return (
 			<div className="book">
 				<JSONTree data={this.props.book} />
+				<h3>Tracks</h3>
+				<ul>
+					{TracksList.map(track =>
+						<li key={track.RecordFileName}>{track.RecordFileName} / {track.RecordTitle}</li>
+					)}
+				</ul>
 			</div>
 		);
 	}
---
+++ books/src/components/Player.tsx
@@ -0,0 +1,163 @@
+import * as React from 'react';
+import ReactHowler from 'react-howler';
+import raf from 'raf';
+
+// const ReactHowlerr: any = ReactHowler;
+
+const Button = ({ children, ...props }) =>
+	<button {...props}>{children}</button>;
+
+class Player extends React.Component<any, any> {
+	player: any;
+	_raf: any;
+
+	state: any = {
+		playing: false,
+		loaded: false,
+		loop: false,
+		mute: false,
+		volume: 1.0
+	}
+
+	componentWillUnmount () {
+		this.clearRAF()
+	}
+
+	handleToggle = () => {
+		this.setState(state => ({
+			...state,
+			playing: !this.state.playing
+		}));
+	}
+
+	handleOnLoad = () => {
+		this.setState({
+			loaded: true,
+			duration: this.player.duration()
+		})
+	}
+
+	handleOnPlay = () => {
+		this.setState({
+			playing: true
+		})
+		this.renderSeekPos()
+	}
+
+	handleOnEnd = () => {
+		this.setState({
+			playing: false
+		})
+		this.clearRAF()
+	}
+
+	handleStop = () => {
+		this.player.stop()
+		this.setState({
+			playing: false // Need to update our local state so we don't immediately invoke autoplay
+		})
+		this.renderSeekPos()
+	}
+
+	handleLoopToggle = () => {
+		this.setState({
+			loop: !this.state.loop
+		})
+	}
+
+	handleMuteToggle = () => {
+		this.setState({
+			mute: !this.state.mute
+		})
+	}
+
+	renderSeekPos () {
+		this.setState({
+			seek: this.player.seek()
+		})
+		if (this.state.playing) {
+			this._raf = raf(this.renderSeekPos)
+		}
+	}
+
+	clearRAF () {
+		// raf.cancel(this._raf)
+		raf.cancel(this._raf)
+	}
+
+	render () {
+		const { bookId, record } = this.props;
+		return (
+			<div className='full-control'>
+				<ReactHowler
+					src={`https://storage.loudbook.ru/storagenew/${bookId}/records/${record}`}
+					playing={this.state.playing}
+					onLoad={this.handleOnLoad}
+					onPlay={this.handleOnPlay}
+					onEnd={this.handleOnEnd}
+					loop={this.state.loop}
+					mute={this.state.mute}
+					volume={this.state.volume}
+					ref={(ref) => (this.player = ref)}
+				/>
+
+				<p>{(this.state.loaded) ? 'Loaded' : 'Loading'}</p>
+
+				<div className='toggles'>
+					<label>
+						Loop:
+						<input
+							type='checkbox'
+							checked={this.state.loop}
+							onChange={this.handleLoopToggle}
+						/>
+					</label>
+					<label>
+						Mute:
+						<input
+							type='checkbox'
+							checked={this.state.mute}
+							onChange={this.handleMuteToggle}
+						/>
+					</label>
+				</div>
+
+				<p>
+					{'Status: '}
+					{(this.state.seek !== undefined) ? this.state.seek.toFixed(2) : '0.00'}
+					{' / '}
+					{(this.state.duration) ? this.state.duration.toFixed(2) : 'NaN'}
+				</p>
+
+				<div className='volume'>
+					<label>
+						Volume:
+						<span className='slider-container'>
+							<input
+								type='range'
+								min='0'
+								max='1'
+								step='.05'
+								value={this.state.volume}
+								onChange={e => this.setState({
+									volume: parseFloat(e.target.value)
+								})}
+								style={{verticalAlign: 'bottom'}}
+							/>
+						</span>
+						{this.state.volume.toFixed(2)}
+					</label>
+				</div>
+
+				<Button onClick={this.handleToggle}>
+					{(this.state.playing) ? 'Pause' : 'Play'}
+				</Button>
+				<Button onClick={this.handleStop}>
+					Stop
+				</Button>
+			</div>
+		)
+	}
+}
+
+export default Player
