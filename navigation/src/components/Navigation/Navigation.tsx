781f2797510b5a37ca403c6e945c88f7e10b880d
gebeto <slavik.nychkalo@gmail.com>
2019-05-03 20:52:19 +0300

    improve nav

--- navigation/src/components/Navigation/Navigation.tsx
+++ navigation/src/components/Navigation/Navigation.tsx
@@ -19,10 +19,12 @@ class Navigation extends React.Component<any, any> {
 	render() {
 		return (
 			<div className="nav">
+			<div className="nav-container">
 				<Item index={0} activeIndex={this.state.activeIndex} onClick={this.handleItemClick} title="Home" color="purple" icon="home" />
 				<Item index={1} activeIndex={this.state.activeIndex} onClick={this.handleItemClick} title="Likes" color="pink" icon="heart" />
 				<Item index={2} activeIndex={this.state.activeIndex} onClick={this.handleItemClick} title="Search" color="yellow" icon="search" />
 				<Item index={3} activeIndex={this.state.activeIndex} onClick={this.handleItemClick} title="Profile" color="teal" icon="user" />
+			</div>
 			</div>
 		);
 	}
